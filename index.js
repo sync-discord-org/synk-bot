c53eaa7f1e5a6df18c780dcfe154b58ccdb90492
Rerispingo <bernardo.o.horta@gmail.com>
2026-07-14 20:39:55 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+// Permissoes concedidas ao bot
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+});
+
+//
+client.once("ready", () => {
+    console.log(`Bot conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN).then();
