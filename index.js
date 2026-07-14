b43f051c100cf675faeaa4c2faaec21b825bb621
Rerispingo <bernardo.o.horta@gmail.com>
2026-07-14 20:47:11 -0300

    feat: add placeholder reply system

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const { Client, GatewayIntentBits } = require("discord.js");
 
 // Permissoes concedidas ao bot
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
-//
+
+
 client.once("ready", () => {
     console.log(`Bot conectado como ${client.user.tag}`);
+});
+
+// Executa cada mensagem
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content.toLowerCase() === "oi") {
+        message.reply("oi");
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN).then();
