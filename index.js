81b14f502fc225c4f45e09664c1ab0660e349dc7
Kiratsuji <132310994+Kiratsuji@users.noreply.github.com>
2026-07-15 22:22:10 -0300

    feat: merge index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 ﻿require("dotenv").config();
+﻿const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config();
 
 const { Client, GatewayIntentBits } = require("discord.js");
 
@@ -10,10 +14,16 @@ const client = new Client({
     ],
 });
 
+if (!fs.existsSync(path.join(__dirname, "database"))) fs.mkdirSync(path.join(__dirname, "database"));
+
 //Events
 require("./events/ready.js")(client);
 require("./events/messageCreate.js")(client);
+<<<<<<< HEAD
 require("./events/rankingEvents.js")(client);
+=======
+require("./events/interactionCreate.js")(client);
+>>>>>>> feat/ticket-system
 
 //Atualização automática das mensagens de ranking a cada 5 minutos
 require("./events/rankingUpdater.js")(client);
